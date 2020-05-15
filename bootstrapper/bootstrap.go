b0b4025f9e71b4650c30f8ca4b665e7ef64934c5
Josh Zarrabi <jzarrabi@pivotal.io>
2020-05-15 19:12:34 -0400

    improve file organization

---
+++ bootstrapper/bootstrap.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"gopkg.in/yaml.v2"
+)
+
+func bootstrap() error {
+	var config map[string]string
+	configFile, err := ioutil.ReadFile("config.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	name := config["buildpack"]
+	templatedPath := filepath.Join("/tmp", name)
+	err = copyTemplateToTempDir(templatedPath)
+
+	if err != nil {
+		return err
+	}
+
+	err = filepath.Walk(templatedPath, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		if strings.HasPrefix(path, filepath.Join(templatedPath, "bin")) ||
+			strings.HasPrefix(path, filepath.Join(templatedPath, "vendor")) ||
+			strings.HasPrefix(path, filepath.Join(templatedPath, ".github")) ||
+			strings.HasPrefix(path, filepath.Join(templatedPath, ".bin")) {
+			return nil
+		}
+
+		buildpackTOML, err := ioutil.ReadFile(path)
+		if err != nil {
+			panic(err)
+		}
+
+		file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(path)
+
+		t := template.Must(template.New("t1").Parse(string(buildpackTOML)))
+
+		err = t.Execute(file, config)
+		if err != nil {
+			panic(err)
+		}
+
+		file.Close()
+		return nil
+
+	})
+	return nil
+
+}
+
+func copyTemplateToTempDir(path string) error {
+	fmt.Println(path)
+	err := os.Mkdir(path, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	err = CopyDirectory("template-cnb", path)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
