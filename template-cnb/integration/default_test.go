bdd65a104b0fe0fdfb0039b5b04cbf3ee37411c2
Genevieve <genlesperance@gmail.com>
2021-04-06 11:25:35 -0700

    Update default_test.go
    Use latest pack convention

--- template-cnb/integration/default_test.go
+++ template-cnb/integration/default_test.go
@@ -55,7 +55,7 @@ func testDefault(t *testing.T, context spec.G, it spec.S) {
 
 			var logs fmt.Stringer
 			image, logs, err = pack.WithNoColor().Build.
-				WithNoPull().
+				WithPullPolicy("never").
 				WithBuildpacks(
 					buildpack,
 				).
