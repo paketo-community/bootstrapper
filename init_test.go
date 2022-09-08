d232f6a797a710d39adebf289278f3878e28b5f1
Ryan Moran <rmoran@vmware.com>
2022-09-08 08:36:33 -0700

    Only build CLI once

--- init_test.go
+++ init_test.go
@@ -4,28 +4,26 @@ import (
 	"testing"
 	"time"
 
-	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
 	"github.com/sclevine/spec"
 	"github.com/sclevine/spec/report"
+
+	. "github.com/onsi/gomega"
 )
 
 var path string
 
 func TestBootstrapperTool(t *testing.T) {
-	// Need the 30 second timeout to run tool, run integration tests and unit tests in created buildpack
 	SetDefaultEventuallyTimeout(60 * time.Second)
 
-	suite := spec.New("Bootstrapper", spec.Report(report.Terminal{}))
-	suite.Before(func(t *testing.T) {
-		var (
-			Expect = NewWithT(t).Expect
-			err    error
-		)
+	var (
+		err    error
+		Expect = NewWithT(t).Expect
+	)
+	path, err = gexec.Build("github.com/paketo-community/bootstrapper", "-ldflags", `-X github.com/paketo-community/bootstrapper/commands.bootstrapperVersion=1.2.3`)
+	Expect(err).NotTo(HaveOccurred())
 
-		path, err = gexec.Build("github.com/paketo-community/bootstrapper", "-ldflags", `-X github.com/paketo-community/bootstrapper/commands.bootstrapperVersion=1.2.3`)
-		Expect(err).NotTo(HaveOccurred())
-	})
+	suite := spec.New("Bootstrapper", spec.Report(report.Terminal{}))
 	suite("Run", testRun)
 	suite("Version", testVersion)
 	suite.Run(t)
